92fb8821c326899dacffcab0acb1d56aa6b65c83
Midhun Balachandran <midhunbalachandran07@gmail.com>
2024-02-07 14:05:58 +0530

    Implemented a new feature in the application that allows users to convert videos to audio. This feature uses the pytube library to download YouTube videos and the moviepy library to extract audio from the downloaded videos. The audio files are saved in the user's preferred directory and can be accessed for download via a new Django view.

--- youtube_downloader/downloader/urls.py
+++ youtube_downloader/downloader/urls.py
@@ -3,4 +3,7 @@ from .  import views
 
 urlpatterns = [
   path('' , views.download , name='download') ,
+  path('get_video_properties/' , views.get_video_properties , name='get_video_properties') ,
+  path('download_video/' , views.download_video , name='download_video') ,
+  path('video_to_audio/' ,views.video_to_audio , name='video_to_audio') ,
 ]
--- youtube_downloader/downloader/views.py
+++ youtube_downloader/downloader/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
-from django.http import HttpResponse , FileResponse
+from django.http import HttpResponse , FileResponse , JsonResponse
 from pytube.exceptions import VideoUnavailable
-from pytube import YouTube
+from pytube import YouTube , Stream
 import os
+from django.conf import settings
+from moviepy.editor import VideoFileClip
 
 
 def download(request):
@@ -13,25 +15,40 @@ def get_video_properties(request):
     link = request.POST['link']
     try:
       video = YouTube(link)
-      stream = video.streams.get_highest_resolution()
+      streams = []
+      for stream in video.streams:
+        if stream.resolution:
+          streams.append({
+            'resolution': stream.resolution,
+          })
+      # Select the stream with the highest resolution
+      stream = max((s for s in video.streams if s.resolution), key=lambda s: s.resolution, default=None)
+      if stream is None:
+        return JsonResponse({
+          'error': 'No streams available',
+        })  
+      quality = stream.resolution
       # Download the video and get the filename
-      filename = stream.download(output_path=settings.MEDIA_ROOT)
+      filename = stream.download(output_path=os.path.join(settings.MEDIA_ROOT, quality))
       # Check if the file exists
       if os.path.exists(filename):
         # Save the filename in the session
         request.session['filename'] = filename
+        request.session['quality'] = quality
         # Get available streams and their properties
         # Get video properties
         title = video.title
         duration = video.length
         thumbnail_url = video.thumbnail_url
-        quality = stream.resolution
+        quality = quality
+        
         # Return a JSON response
         return JsonResponse({
           'title': title,
           'duration': duration,
           'thumbnail_url': thumbnail_url,
-          'quality': quality,
+          'quality' : quality ,
+          'streams': streams,
         })
       else:
         return JsonResponse({
@@ -43,11 +60,37 @@ def get_video_properties(request):
       })
 
 def download_video(request):
-  # Get the filename from the session
+  quality = request.session.get('quality')
+  print(quality)
   filename = request.session.get('filename')
-  filepath = os.path.join(settings.MEDIA_ROOT, filename)
-  # Open the file in read mode
-  file = open(filepath, 'rb')
-  # Create a FileResponse instance and return it
-  response = FileResponse(file, as_attachment=True)
-  return response
+  if quality is None or filename is None:
+    return JsonResponse({'error': 'No video quality or filename has been set yet'})
+  # Use the quality to determine the correct filepath
+  filepath = os.path.join(settings.MEDIA_ROOT, quality, filename)
+  try:
+    # Open the file in read mode
+    file = open(filepath, 'rb')
+    # Create a FileResponse instance and return it
+    response = FileResponse(file, as_attachment=True)
+    return response
+  except FileNotFoundError:
+    return JsonResponse({'error': 'The requested video file does not exist'})
+
+def video_to_audio(request):
+  filename = request.session.get('filename')
+  if filename is None:
+    return JsonResponse({'error' : 'No video filename has been set yet'})
+  video_clip = VideoFileClip(filename)
+  audio_clip = video_clip.audio
+  audio_filename = filename.rsplit(".", 1)[0] + ".mp3"
+  audio_clip.write_audiofile(audio_filename)   
+  request.session['audio_filename'] = audio_filename
+  audiopath = os.path.join(settings.MEDIA_ROOT , audio_filename)
+  try:
+    # Open the file in read mode
+    file = open(audiopath, 'rb')
+    # Create a FileResponse instance and return it
+    response = FileResponse(file, as_attachment=True)
+    return response
+  except FileNotFoundError:
+    return JsonResponse({'error': 'The requested video file does not exist'})
